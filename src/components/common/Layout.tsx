4d915726b12f2f813daa78b4198b9157910e56ca
Oscdivin <divineuwandu84@Gmail.com>
2024-01-14 23:54:13 +0100

    footer is ready now

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "../Static/Footer";
 
 const Layout = () => {
   return (
     <div>
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
